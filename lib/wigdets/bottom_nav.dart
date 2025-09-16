a5b11f8904ae76fac0951f2f1a7adf50169b705c
Dady  Utenga <dadyutenga@yahoo.com>
2025-09-16 10:47:43 +0300

    Refactor BottomNav to use BottomNavigationBar for improved navigation and UI consistency

--- lib/wigdets/bottom_nav.dart
+++ lib/wigdets/bottom_nav.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:getwidget/getwidget.dart';
 import '../core/theme.dart';
 
 class BottomNav extends StatelessWidget {
@@ -11,81 +10,35 @@ class BottomNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: AppColors.backgroundWhite,
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.textSecondary.withOpacity(0.1),
-            blurRadius: 4,
-            offset: const Offset(0, -1),
-          ),
-        ],
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      backgroundColor: Colors.white,
+      selectedItemColor: AppColors.primaryBlue,
+      unselectedItemColor: Colors.grey,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      elevation: 0,
+      selectedLabelStyle: const TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
       ),
-      child: SafeArea(
-        top: false, // Don't add top padding
-        child: Container(
-          height: 56, // Standard bottom nav height
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildNavItem(index: 0, icon: CupertinoIcons.home, label: 'Home'),
-              _buildNavItem(
-                index: 1,
-                icon: CupertinoIcons.clock,
-                label: 'History',
-              ),
-              _buildNavItem(
-                index: 2,
-                icon: CupertinoIcons.person_circle,
-                label: 'Profile',
-              ),
-            ],
-          ),
+      unselectedLabelStyle: const TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+      ),
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: 'Home'),
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.clock),
+          label: 'History',
         ),
-      ),
-    );
-  }
-
-  Widget _buildNavItem({
-    required int index,
-    required IconData icon,
-    required String label,
-  }) {
-    final isSelected = currentIndex == index;
-
-    return Flexible(
-      child: GestureDetector(
-        onTap: () => onTap(index),
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                icon,
-                color: isSelected
-                    ? AppColors.primaryBlue
-                    : AppColors.textSecondary,
-                size: 24,
-              ),
-              const SizedBox(height: 4),
-              Text(
-                label,
-                style: AppTextStyles.poppins(
-                  fontSize: 11,
-                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.w500,
-                  color: isSelected
-                      ? AppColors.primaryBlue
-                      : AppColors.textSecondary,
-                ),
-              ),
-            ],
-          ),
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.person_circle),
+          label: 'Profile',
         ),
-      ),
+      ],
     );
   }
 }
